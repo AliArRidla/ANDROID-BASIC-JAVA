c9ba1f5acc75335e5b3fc6e89334d030b86c543a
zero0dayali <beeunik0@gmail.com>
2021-01-07 17:08:06 +0700

    sudah menambah / menggunakan Data Binding

--- app/src/main/java/com/belajar/databinding/MainActivity.java
+++ app/src/main/java/com/belajar/databinding/MainActivity.java
@@ -1,27 +1,23 @@
 package com.belajar.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.belajar.databinding.databinding.ActivityMainBinding;
 import com.belajar.databinding.models.Siswa;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Siswa siswa;
-    private TextView txtNama,txtNim;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setSiswa();
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        Siswa siswa = new Siswa("Ali","1931710069");
+        binding.setSiswa(siswa);
     }
-    public void setSiswa(){
-        siswa = new Siswa("Ali","1931710069");
-        txtNama = findViewById(R.id.txtNama);
-        txtNim = findViewById(R.id.txtNim);
-        txtNama.setText(siswa.getNama());
-        txtNim.setText(siswa.getNim());
-    }
+
+
 }
